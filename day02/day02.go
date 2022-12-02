7e7ec795e9bcd6fdd5d470d496b57b3fc86a42ce
Patrick <pvilchez794@gmail.com>
2022-12-02 01:38:05 -0500

    Day02 challenge complete

---
+++ day02/day02.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	values = map[string]int{
+		"A": 1, // Rock
+		"B": 2, // Paper
+		"C": 3, // Scissors
+
+		"X": 1, // Rock
+		"Y": 2, // Paper
+		"Z": 3, // Scissors
+	}
+	scheme = map[string][]string{
+		"A": {"Z", "X", "C", "B"}, // A: Wins, Draw | Wins, Lose
+		"C": {"Y", "Z", "B", "A"}, // C: Wins, Draw | Wins, Lose
+		"B": {"X", "Y", "A", "C"}, // B: Wins, Draw | Wins, Lose
+	}
+)
+
+// Calculate the score with the rules of first part of challenge
+func firstPart(round []string) int {
+	// You lose
+	if scheme[round[0]][0] == round[1] {
+		return 0 + values[round[1]]
+	}
+
+	// You draw
+	if scheme[round[0]][1] == round[1] {
+		return 3 + values[round[1]]
+	}
+
+	// You win
+	return 6 + values[round[1]]
+}
+
+// Calculate the score with the rules of second part of challenge
+func secondPart(round []string) int {
+	// You lose
+	if round[1] == "X" {
+		return 0 + values[scheme[round[0]][2]]
+	}
+
+	// You draw
+	if round[1] == "Y" {
+		return 3 + values[scheme[round[0]][1]]
+	}
+
+	// You win
+	return 6 + values[scheme[round[0]][3]]
+}
+
+func main() {
+	file, err := os.Open("strategy.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	points := 0
+	secondPoints := 0
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		round := strings.Split(scanner.Text(), " ")
+
+		points = points + firstPart(round)
+		secondPoints = secondPoints + secondPart(round)
+	}
+
+	fmt.Println(points)
+	fmt.Println(secondPoints)
+}
