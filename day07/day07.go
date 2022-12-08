c7730d5a482bc076a60dc72bd80a4743f77b6483
Patrick <pvilchez794@gmail.com>
2022-12-08 00:32:05 -0500

    Day07 complete, refactor

---
+++ day07/day07.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
+
+func sumFileSizes(dir []string, dirs [][]string) int {
+	count := 0
+
+	for _, d := range dir[1:] {
+		i, err := strconv.Atoi(d)
+		if err != nil {
+			for idx, f := range dirs {
+				if d == f[0] {
+					count += sumFileSizes(f, dirs[idx+1:])
+					break
+				}
+			}
+			continue
+		}
+
+		count += i
+	}
+
+	return count
+}
+
+func main() {
+	file, err := os.ReadFile("ex.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	commands := strings.Split(string(file), "\n")
+
+	dir := make([]string, 0)
+	dirs := make([][]string, 0)
+
+	depth := 0
+
+	for idx, c := range commands {
+		line := strings.Split(c, " ")
+
+		if line[0] == "$" {
+			if line[1] == "ls" {
+				continue
+			}
+
+			if line[1] == "cd" {
+				if line[2] == ".." {
+					depth--
+					continue
+				}
+
+				if len(dir) > 0 {
+					dirs = append(dirs, dir)
+					dir = make([]string, 0)
+				}
+
+				dir = append(dir, line[2])
+				depth++
+				continue
+			}
+		}
+
+		if line[0] == "dir" {
+			dir = append(dir, line[1])
+			continue
+		}
+
+		dir = slices.Insert(dir, 1, line[0])
+
+		if idx == len(commands)-1 {
+			dirs = append(dirs, dir)
+			break
+		}
+	}
+
+	result := make([]int, 0)
+
+	for idx, d := range dirs {
+		result = append(result, sumFileSizes(d, dirs[idx+1:]))
+	}
+
+	count := 0
+
+	for _, r := range result {
+		if r <= 100000 {
+			count += r
+		}
+	}
+
+	res := 0
+
+	for _, d := range dirs {
+		for _, f := range d {
+			i, err := strconv.Atoi(f)
+			if err != nil {
+				continue
+			}
+
+			res += i
+		}
+	}
+
+	unusedSpaceDisk := 70000000 - res
+	sol := 0
+
+	slices.Sort(result)
+	slices.SortFunc(result, func(a, b int) bool {
+		return a > b
+	})
+
+	for idx, r := range result {
+		if unusedSpaceDisk+r < 30000000 {
+			sol += result[idx-1]
+			break
+		}
+	}
+
+	fmt.Println(result)
+	fmt.Println(count)
+
+	fmt.Println(res)
+	fmt.Println(unusedSpaceDisk)
+	fmt.Println(sol)
+}
