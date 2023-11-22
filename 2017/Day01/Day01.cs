725abaa1dbfd03375199d73ccc3cf5f8436637dd
Patrick Vilchez <pvilchez794@gmail.com>
2023-11-22 01:05:59 -0500

    Day 1 complete

---
+++ 2017/Day01/Day01.cs
@@ -0,0 +1,65 @@
+﻿namespace _2017.Day01;
+
+public class Day01
+{
+    private readonly string _filepath = Path.Join(".", "Day01", "input.txt");
+    
+    public void Part01()
+    {
+        var reader = new StreamReader(_filepath);
+        var character = reader.Read();
+
+        var firstDigit = -1;
+        var sumDigit = 0;
+
+        while (character != -1)
+        {
+            var intDigit = int.Parse(((char)character).ToString());
+            var intNextDigit = reader.Peek() == -1 ? -1 : int.Parse(((char)reader.Peek()).ToString());
+            
+            if (firstDigit < 0) firstDigit = intDigit;
+
+            if (intNextDigit > 0)
+            {
+                sumDigit += intDigit == intNextDigit ? intDigit : 0;
+            }
+            else
+            {
+                sumDigit += intDigit == firstDigit ? intDigit : 0;
+            }
+            
+            character = reader.Read();
+        }
+        
+        reader.Close();
+        
+        Console.WriteLine($"Sum of all digits: {sumDigit}");
+    }
+
+    public void Part02()
+    {
+        var digits = File.ReadAllText(_filepath);
+        
+        var sumDigit = 0;
+        var half = digits.Length / 2;
+
+        for (var i = 0; i < digits.Length; i++)
+        {
+            // Max digits length
+            if (i + half > digits.Length - 1)
+            {
+                if (digits[i] == digits[i + half - (digits.Length - 1) - 1])
+                {
+                    sumDigit += int.Parse(digits[i].ToString());
+                }
+            }
+            // Standard digit length
+            else
+            {
+                if (digits[i] == digits[i + half]) sumDigit += int.Parse(digits[i].ToString());
+            }
+        }
+        
+        Console.WriteLine($"Sum of all digits: {sumDigit}");
+    }
+}
--- 2017/Program.cs
+++ 2017/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace _2017;
+
+internal abstract class _2017
+{
+    private static void Main(string[] args)
+    {
+        var day01 = new Day01.Day01();
+        
+        switch (int.Parse(args[0]))
+        {
+            case 1:
+                day01.Part01();
+                day01.Part02();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+    }
+}
