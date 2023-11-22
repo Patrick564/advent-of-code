5f1bb495612105dd0e1a32cc2ccb4088fcea1a98
Patrick Vilchez <pvilchez794@gmail.com>
2023-11-22 12:32:12 -0500

    Day 2 complete

---
+++ 2017/Day02/Day02.cs
@@ -0,0 +1,64 @@
+﻿namespace _2017.Day02;
+
+public class Day02
+{
+    private readonly string _filepath = Path.Join(".", "Day02", "input.txt");
+
+    public void Part01()
+    {
+        var reader = new StreamReader(_filepath);
+        var line = reader.ReadLine();
+
+        var checksum = 0;
+
+        while (line != null)
+        {
+            var smallest = int.MaxValue;
+            var largest = int.MinValue;
+
+            foreach (var c in line.Split("\t"))
+            {
+                if (int.Parse(c) > largest) largest = int.Parse(c);
+                
+                if (int.Parse(c) < smallest) smallest = int.Parse(c);
+
+            }
+
+            checksum += largest - smallest;
+            line = reader.ReadLine();
+        }
+        
+        reader.Close();
+        
+        Console.WriteLine($"Checksum: {checksum}");
+    }
+
+    public void Part02()
+    {
+        var reader = new StreamReader(_filepath);
+        var line = reader.ReadLine();
+
+        var checksum = 0;
+
+        while (line != null)
+        {
+            var numbers = line.Split("\t").Select(int.Parse).ToArray();
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                for (var j = 0; j < numbers.Length; j++)
+                {
+                    if (i == j) continue;
+                    
+                    if (numbers[i] % numbers[j] == 0) checksum += numbers[i] / numbers[j];
+                }
+            }
+            
+            line = reader.ReadLine();
+        }
+        
+        reader.Close();
+        
+        Console.WriteLine($"Checksum of module: {checksum}");
+    }
+}
--- 2017/Program.cs
+++ 2017/Program.cs
@@ -5,6 +5,7 @@ internal abstract class _2017
     private static void Main(string[] args)
     {
         var day01 = new Day01.Day01();
+        var day02 = new Day02.Day02();
         
         switch (int.Parse(args[0]))
         {
@@ -13,6 +14,8 @@ internal abstract class _2017
                 day01.Part02();
                 break;
             case 2:
+                day02.Part01();
+                day02.Part02();
                 break;
             case 3:
                 break;
