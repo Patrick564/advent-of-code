aa07ccd1a2553569039579581c36c6b04d312689
Patrick Vilchez <pvilchez794@gmail.com>
2023-12-01 15:47:44 -0500

    Day 01 update

--- 2023/Day01/Day01.cs
+++ 2023/Day01/Day01.cs
@@ -5,11 +5,25 @@ namespace _2023.Day01;
 public class Day01
 {
     private readonly string _filePath = Path.Join(".", "Day01", "input.txt");
+    
+    private readonly Dictionary<string, string> _equivalence = new()
+    {
+        { "one", "1" },
+        { "two", "2" },
+        { "three", "3" },
+        { "four", "4" },
+        { "five", "5" },
+        { "six", "6" },
+        { "seven", "7" },
+        { "eight", "8" },
+        { "nine", "9" }
+    };
 
     public void Part01()
     {
         var calibrationValues = File.ReadAllLines(_filePath);
         var rx = new Regex("[0-9]");
+        
         var calibrationSum = calibrationValues
             .Select(value => rx.Matches(value))
             .Select(match => int.Parse(match.First().Value + match.Last().Value))
@@ -20,45 +34,34 @@ public class Day01
 
     public void Part02()
     {
-        var calibrationValues = File.ReadAllLines(_filePath);
-        var eq = new Dictionary<string, int>
-        {
-            { "one", 1 },
-            { "two", 2 },
-            { "three", 3 },
-            { "four", 4 },
-            { "five", 5 },
-            { "six", 6 },
-            { "seven", 7 },
-            { "eight", 8 },
-            { "nine", 9 }
-        };
-        var calibrationSum = 0;
+        var calibration = File.ReadAllLines(_filePath);
+        
+        var totalSum = 0;
 
-        foreach (var value in calibrationValues)
+        foreach (var value in calibration)
         {
-            var values = new List<int>();
+            var values = new List<string>();
             
             for (var i = 0; i < value.Length; i++)
             {
                 if (int.TryParse(value[i].ToString(), out var digit))
                 {
-                    values.Add(digit);
+                    values.Add(digit.ToString());
                     continue;
                 }
 
                 for (var j = i + 1; j < value.Length; j++)
                 {
-                    if (eq.TryGetValue(value[i] + value.Substring(i + 1, j - i), out var word))
+                    if (_equivalence.TryGetValue(value[i] + value.Substring(i + 1, j - i), out var word))
                     {
                         values.Add(word);
                     }
                 }
             }
             
-            calibrationSum += values.First() + values.Last();
+            totalSum += int.Parse(values.First() + values.Last());
         }
         
-        Console.WriteLine($"Calibration values sum: {calibrationSum}");
+        Console.WriteLine($"Calibration values sum: {totalSum}");
     }
 }
